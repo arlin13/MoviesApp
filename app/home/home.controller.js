e0ba1af426ac7f52d0ce5211ef02924ef0e2e780
Arlin <arlin.grijalba@gmail.com>
2017-03-17 10:01:24 -0700

    Installed ngMap. Showing map on index.html

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -5,12 +5,24 @@
         .module('myApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['omdbFactory'];
+    HomeController.$inject = ['omdbFactory', 'NgMap'];
 
     /* @ngInject */
-    function HomeController(omdbFactory) {
+    function HomeController(omdbFactory, NgMap) {
         var vm = this;
-        vm.movies=[];
+        vm.movies = [];
+
+        ///////////////
+        NgMap.getMap().then(function(map) {
+            console.log(map.getCenter());
+            console.log('markers', map.markers);
+            console.log('shapes', map.shapes);
+        });
+        ///////////////
+
+        vm.clearControl = function clearControl() {
+            vm.movieName = '';
+        }
 
         vm.searchMovie = function searchMovie() {
             omdbFactory
@@ -18,16 +30,18 @@
                 .then(function(data) {
                     vm.movies = data.Search;
                     console.log(vm.movies);
+                    vm.clearControl();
                 })
                 .catch(function(error) {
                     console.log(error);
                 })
         }
 
-        vm.searchMovie("sponge bob");
+        // vm.searchMovie("sponge bob");
 
         activate();
 
         function activate() {}
     }
+
 })();
